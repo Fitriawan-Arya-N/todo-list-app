3634735c711bc59119b703ce5e33347bd0bd0d07
Fitriawan-Arya-N <nugrahaaryaa17@gmail.com>
2025-01-14 17:23:47 +0700

    update

--- todo-list-backend/routes/todos.js
+++ todo-list-backend/routes/todos.js
@@ -1,31 +1,40 @@
 const express = require('express');
-const db = require('../db/connection');
+const db = require('../db/connection'); // Koneksi DB
 const router = express.Router();
 
 // Get all todos
-router.get('/', (req, res) => {
-    db.query('SELECT * FROM todos', (err, results) => {
-        if (err) throw err;
+router.get('/', async (req, res) => {
+    try {
+        const [results] = await db.query('SELECT * FROM todos');
         res.json(results);
-    });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error retrieving todos');
+    }
 });
 
 // Add new todo
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     const { title, completed } = req.body;
-    db.query('INSERT INTO todos (title, completed) VALUES (?, ?)', [title, completed], (err) => {
-        if (err) throw err;
+    try {
+        await db.query('INSERT INTO todos (title, completed) VALUES (?, ?)', [title, completed]);
         res.status(201).send('Todo added');
-    });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error adding todo');
+    }
 });
 
 // Delete a todo
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
     const { id } = req.params;
-    db.query('DELETE FROM todos WHERE id = ?', [id], (err) => {
-        if (err) throw err;
+    try {
+        await db.query('DELETE FROM todos WHERE id = ?', [id]);
         res.send('Todo deleted');
-    });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error deleting todo');
+    }
 });
 
 module.exports = router;
